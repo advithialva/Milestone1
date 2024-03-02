27b12d66fef3d77ffd6efc5abcdb621cf54e9a8f
advithialva <advithialva2004@gmail.com>
2024-03-02 16:27:46 +0530

    first-commit

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import img1 from "../assets/img1.jpeg";
+import img2 from "../assets/img2.jpeg";
+import img3 from "../assets/img3.jpeg";
+import img4 from "../assets/img4.jpeg";
+import img5 from "../assets/img5.jpeg";
+import img6 from "../assets/img6.jpeg";
+
+export const MenuList = [
+  {
+    name: "King Fish Fry",
+    image: img1,
+    price: 399,
+  },
+  {
+    name: "Mysuru Masala Dosa",
+    image: img2,
+    price: 80,
+  },
+  {
+    name: "Coastal Chicken Biriyani",
+    image: img3,
+    price: 290,
+  },
+  {
+    name: "Tava Fried Prawns",
+    image: img4,
+    price: 399,
+  },
+  {
+    name: "Fish Thali",
+    image: img5,
+    price: 350,
+  },
+  {
+    name: "Squid Ghee Roast",
+    image: img6,
+    price: 450,
+  },
+];
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/banner.jpeg";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+      <div className="headerContainer">
+      <h1 style={{ color: "black", fontFamily: "Roboto, sans-serif", fontWeight: 700 }}>Coastal Delights</h1>
+        <p style={{ color: "black", fontFamily: "sans-serif" }}>Where Tradition Meets Taste</p>
+        <Link to="/menu">
+          <button> ORDER NOW </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
